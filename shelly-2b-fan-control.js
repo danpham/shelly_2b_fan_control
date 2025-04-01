45872e3ad25251eb0aa5783d8696aba384b0073a
Dan Pham <dan@danpham.com>
2025-04-01 18:10:54 +0200

    Move files.

--- shelly-2b-fan-control.js
+++ shelly-2b-fan-control.js
@@ -1,20 +1,3 @@
-/* Configuration dans l'application pour chaque couple entrée/sortie :  
-- Sélectionner le bouton d'entrée/le mode de l'interrupteur  
-  Interrupteur  
-- Définir le type de relais  
-  Régler le dispositif Shelly pour qu'il soit en mode interrupteur "autonome"  
-- Définir l'alimentation du relais par défaut  
-  IO1 : Configurer le dispositif Shelly pour qu'il restaure le dernier mode dans lequel il se trouvait lorsqu'il est alimenté  
-  Configuration réinitialisation  
-  IO2 : Configurer le dispositif Shelly pour qu'il s'éteigne lorsqu'il est alimenté  
-- Décocher "Activation de la réinitialisation à partir de l'entrée" : IO1 et IO2  
-
-Factory reset by input est un moyen de restaurer les paramètres d'usine du dispositif et d'annuler toutes les modifications effectuées. Cette action est possible uniquement dans les 60 premières secondes après le démarrage. L'utilisateur doit basculer l'entrée 5 fois, et le dispositif sera réinitialisé à ses paramètres d'usine. La réinitialisation d'usine peut également être effectuée via un appel RPC.  
-
-- Scripts  
-  Cocher "Run on startup"  
-*/
-
 /* Inputs */
 let LIGHT_INPUT_ID = 0;
 let FAN_INPUT_ID = 1;
